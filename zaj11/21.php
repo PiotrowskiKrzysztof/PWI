cd0bf561021228ef724055d1a79f22d987e97495
Krzysztof <47855045+PiotrowskiKrzysztof@users.noreply.github.com>
2019-05-23 13:24:21 +0200

    Update 21.php

--- zaj11/21.php
+++ zaj11/21.php
@@ -2,24 +2,24 @@
 
    try
    {
-    if(isset($_GET['id'])) {
+    if(isset($_GET['id'])) { // zabezpieczenie do podania id
         $dbalkohole = new PDO('mysql:host=localhost;dbname=alkohole', 'root', '');
         $dbalkohole->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $stm = $dbalkohole->prepare("SELECT * FROM alkohole WHERE id = ?");
-        $stm->execute(array($_GET['id']));
-
-        $res = $stm->fetchAll();
-        if(count($res) != 0) {
+        $stm->execute(array($_GET['id'])); // wykonuje zadane polecenie
+        $stm->setFetchMode(PDO::FETCH_ASSOC); // pobieranie tylko tablicy asocjacyjnej
+        $res = $stm->fetchAll(); // lapie caly wynik zpaytania
+        if(count($res) != 0) { // zabezpieczenie odnoszace sie do zawartosic tablicy
         foreach($res as $row) {
             //var_dump($row);
             foreach($row as $value) {
                 echo $value . "\t";
             }
         }
-    } else {
-        echo 'Brak wynikow!';
-    }
+        } else {
+            echo 'Brak wynikow!';
+        }
     } else {
         echo 'Nie podano id.';
     }
